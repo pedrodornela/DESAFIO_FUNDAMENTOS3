402b8b896c4267c3f4384e32575aa48ab6230602
Pedro Henrique Dornela Gonçalves <pedro.henriquedgoncalves@gmail.com>
2024-11-09 21:04:15 -0300

    first commit

---
+++ Models/Nokia.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DESAFIO_FUNDAMENTOS3.Models
+{
+    public class Nokia : Smartphone
+    {
+
+        public Nokia(string numero, string modelo, string imei, int memoria):base(numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo '{nomeApp}' no Nokia...");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using DESAFIO_FUNDAMENTOS3.Models;
+
+
+Console.WriteLine("Smartphone IPHONE: ");
+Smartphone iphone = new Iphone(numero: "(88) 3435-4234", modelo: "Modelo NK1", imei: "652314897", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone NOKIA: ");
+Smartphone nokia = new Nokia(numero: "(63) 2657-3527", modelo: "MODELO I2024", imei: "253698417", memoria: 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
